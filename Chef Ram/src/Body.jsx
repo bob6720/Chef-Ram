8049932fee206782dd6bbddcc441a2ea81148c6f
bob6720 <meyerharry0@gmail.com>
2025-01-14 13:46:30 +1300

    stage 1

--- Chef Ram/src/Body.jsx
+++ Chef Ram/src/Body.jsx
@@ -1,18 +1,21 @@
+import React from "react"
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe"
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai"
+
 export default function Body() {
-    const ingredients = ["eggs", "flour", "sugar"];
-
-    const ingredientList = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ));
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        console.log("Form submitted!");
+    const [ingredients, setIngredients] = React.useState([groundBeef, onions, garlic])
+    const [recipeShown, setRecipeShown] = React.useState(false)
+    function toggleRecipeShown() {
+        setRecipeShown(prev=>!prevShown)
+    }
+    function addIngredient(formData) {
+        const setIngredients = [...prevIngredients, newIngredient]
     }
 
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
+            <form onSubmit={addIngredient} className="add-ingredient-form">
                 <input 
                 type="text" 
                 aria-label="Add ingredient"
@@ -22,7 +25,11 @@ export default function Body() {
                 <button>Add ingredient</button>
             </form>
             <ul>
-                {ingredientList}
+                {ingredient.length > 0 && 
+                <IngredientsList
+                Ingredients={ingredients}
+                toggleRecipeShown={toggleRecipeShown} 
+                />}
             </ul>
         </main>
     );
